cf1aece7d1882fa7c893613c0f7ba569a927ad25
Julie Boehm <julie.anne.boehm@gmail.com>
2022-04-13 16:09:37 -0600

    commit tree c1996a1b91b0647dd7c99a9966d442cd6d6a5590

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function introduction(name) {
+    return(`Hi, my name is ${name}.`) 
+} 
+introduction();
+
+
+
+function introductionWithLanguage(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`) 
+}
+introductionWithLanguage();
+
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+   return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    
+  }
+  introductionWithLanguageOptional()
+
+ 
+
+function introductionWithLanguageOptional(name = User, language = "JavaScript") {
+   return(`Hi, my name is ${name} and I am learning to program in ${language}.`) 
+ 
+}
+introductionWithLanguageOptional(); 
